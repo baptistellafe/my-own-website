e397dca6c9b78cbe4d7a3c3386d93efd663a2d7f
felipe.ferreiro <felipe.vieira@mjv.com.br>
2020-10-11 03:37:54 -0300

    Iniciando a criação do meu portfólio virtual

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { AppComponent } from './app.component';
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
