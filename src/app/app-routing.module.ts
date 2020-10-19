28837e6e83ca4f259a58be3576af05cc6f76dfed
felipe.ferreiro <felipe.vieira@mjv.com.br>
2020-10-19 16:31:22 -0300

    Alterando rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'what-i-do',
+    redirectTo: 'coding',
     pathMatch: 'full'
 
   },
